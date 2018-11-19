7698ecf21d9a1f705a82c39c8a33c47405f8ec5f
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-11-19 11:52:28 +0530

    herok config2

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cms-frontend'));
+app.use(express.static(__dirname + '/dist/cmsforntend-ck'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/cms-frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cmsforntend-ck/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
